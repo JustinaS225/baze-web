f9eab0a4dbf884203303c303b0582d7cfe60ac88
JustinaS225 <109916101+JustinaS225@users.noreply.github.com>
2024-02-09 19:42:44 +0200

    did

--- src/app/[lang]/apzvalgos/[id]/page.tsx
+++ src/app/[lang]/apzvalgos/[id]/page.tsx
@@ -1,21 +1,30 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import { getAssetsReusable } from "@/utils/firebase/getAssetsReusable";
-import { IAssetsCollection } from "@/interfaces/firestore";
+import { IAssetsCollection, IAssetsDynamicSubcollection } from "@/interfaces/firestore";
+import Review from "../../components/Review";
 
 
 type Props = {
   params: {
     lang: string;
-  }
+    id: string;
+  },
 }
+
+export async function generateStaticParams({params}: Props) {
+  const data: IAssetsCollection = await getAssetsReusable(params.lang, 'reviews', ['albums']);
+  return data.dynamic.albums.slugs.map((slug: string) => ({id: slug}));
+}
+
+export const dynamicParams = false;
 
 const ReviewPage = async ({params}: Props) => {
   const data: IAssetsCollection = await getAssetsReusable(params.lang, 'reviews', ['albums']);
 
   return (
     <div className={styles.wrap}>
-      DINAMIŠKA JAUNATVIŠKA SUSTAINABLE
+      <Review data={data}/>
     </div>
   );
 }
